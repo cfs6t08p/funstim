1fb07bc5810b2e98ccdd471a61ffcbfa92cf4651
cfs6t08p <45593096+cfs6t08p@users.noreply.github.com>
2018-12-07 00:52:43 +0100

    Add files via upload

---
+++ funstim.js
@@ -0,0 +1,101 @@
+importScripts("lame.min.js");
+
+onmessage = e => {
+  e = e.data;
+  let msg = {};
+  
+  msg.pctDone = 0;
+  msg.status = "primary";
+
+  let fs = JSON.parse(e.funscript);
+  
+  if(!fs.actions || fs.actions.length == 0) {
+    throw "No actions defined in funscript";
+  }
+  
+  if(fs.inverted) {
+    e.polarity = -e.polarity;
+  }
+  
+  let actions = fs.actions;
+  let length = actions.slice(-1).pop().at;
+  
+  let a1 = actions[0];
+  let a0 = a1;
+  
+  let mp3encoder = new lamejs.Mp3Encoder(2, e.sampleRate, e.bitrate);
+  let mp3Data = [];
+  
+  let sampleBlockSize = 1152;
+  let left = new Int16Array(sampleBlockSize);
+  let right = new Int16Array(sampleBlockSize);
+  
+  let radsPerSample = e.frequency.map(f => ((f * Math.PI * 2) / e.sampleRate));
+  let samplesPerMs = e.sampleRate / 1000;
+  let normalSamples = length * samplesPerMs;
+  let fadeSamples = e.fade * samplesPerMs;
+  let totalSamples = normalSamples + fadeSamples;
+  let sample = 0;
+  let actionIndex = 0;
+  
+  actions = actions.map(a => ({at: a.at * samplesPerMs, pos: a.pos / 100}));
+  
+  while(sample < totalSamples) {
+    for(let i = 0; i < sampleBlockSize; i++) {
+      if(a1.at < sample && actionIndex < actions.length - 1) {
+        a0 = a1;
+        actionIndex++;
+        a1 = actions[actionIndex];
+      }
+      
+      let dist = a1.at - a0.at;
+      let dpos = a1.pos - a0.pos;
+      
+      let alpha = Math.max(0, Math.min(1, (sample - a0.at) / dist)) || 0;
+      let pos = Math.max(0, Math.min(1, a0.pos + dpos * alpha));
+      
+      let amp = 1;
+      
+      if(sample < fadeSamples) {
+        amp = sample / fadeSamples;
+      } else if(sample >= normalSamples) {
+        amp = 1 - (sample - normalSamples) / fadeSamples;
+      }
+      
+      let inphase = radsPerSample.reduce((a, c) => a + Math.sin(sample * c), 0) / radsPerSample.length;
+      let outphase = radsPerSample.reduce((a, c) => a + e.polarity * Math.sin(sample * c + pos * Math.PI), 0) / radsPerSample.length;
+      
+      left[i] = inphase * 32767 * amp * e.amplitude;
+      right[i] = outphase * 32767 * amp * e.amplitude;
+      
+      sample++;
+    }
+    
+    let mp3buf = mp3encoder.encodeBuffer(left, right);
+    if (mp3buf.length > 0) {
+      mp3Data.push(mp3buf);
+    }
+    
+    let newPct = Math.trunc((sample / totalSamples) * 100);
+    
+    if(newPct > msg.pctDone) {
+      msg.pctDone = newPct;
+      
+      postMessage(msg);
+    }
+  }
+  
+  let mp3buf = mp3encoder.flush();
+  
+  if (mp3buf.length > 0) {
+    mp3Data.push(mp3buf);
+  }
+  
+  msg.mp3Data = mp3Data;
+  msg.pctDone = 100;
+  msg.status = "success";
+  
+  postMessage(msg);
+  
+  close();
+}
