5fab8d6078f6b67af603f6a30d660d72c550cd8a
cfs6t08p <45593096+cfs6t08p@users.noreply.github.com>
2019-01-22 21:34:51 +0100

    v2.0

--- funstim.js
+++ funstim.js
@@ -20,8 +20,12 @@ onmessage = e => {
   let actions = fs.actions;
   let length = actions.slice(-1).pop().at;
   
-  let a1 = actions[0];
-  let a0 = a1;
+  if(e.fullrange) {
+    let min = actions.map(a => a.pos).reduce((a, c) => (a < c ? a : c), 100);
+    let max = actions.map(a => a.pos).reduce((a, c) => (a > c ? a : c), 0);
+  
+    actions = actions.map(a => ({ pos: ((a.pos - min) / (max - min)) * 100, at: a.at }));
+  }
   
   let mp3encoder = new lamejs.Mp3Encoder(2, e.sampleRate, e.bitrate);
   let mp3Data = [];
@@ -34,11 +38,16 @@ onmessage = e => {
   let samplesPerMs = e.sampleRate / 1000;
   let normalSamples = length * samplesPerMs;
   let fadeSamples = e.fade * samplesPerMs;
-  let totalSamples = normalSamples + fadeSamples;
+  let totalSamples = normalSamples + fadeSamples * 2;
   let sample = 0;
   let actionIndex = 0;
   
   actions = actions.map(a => ({at: a.at * samplesPerMs, pos: a.pos / 100}));
+  
+  let a1 = actions[0];
+  let a0 = a1;
+  
+  let ampFilter = 0;
   
   while(sample < totalSamples) {
     for(let i = 0; i < sampleBlockSize; i++) {
@@ -54,19 +63,46 @@ onmessage = e => {
       let alpha = Math.max(0, Math.min(1, (sample - a0.at) / dist)) || 0;
       let pos = Math.max(0, Math.min(1, a0.pos + dpos * alpha));
       
-      let amp = 1;
+      if(e.doubletime) {
+        pos = Math.abs((alpha * 2) - 1);
+      }
+      
+      let amp = 0;
+      
+      if(e.fadeonpause && fadeSamples && sample >= a0.at && sample <= a1.at) {
+        let d0 = sample - a0.at;
+        let d1 = a1.at - sample;
+        let dmin = d0 < d1 ? d0 : d1;
+        
+        if(dmin > fadeSamples) {
+          dmin = fadeSamples;
+        }
+        
+        amp = (fadeSamples - dmin) / fadeSamples;
+      }
       
       if(sample < fadeSamples) {
-        amp = sample / fadeSamples;
+        amp = Math.min(amp, sample / fadeSamples);
       } else if(sample >= normalSamples) {
-        amp = 1 - (sample - normalSamples) / fadeSamples;
+        amp = Math.max(0, Math.min(amp, 1 - (sample - normalSamples) / fadeSamples));
       }
+      
+      let filterLength = (e.sampleRate * e.fade) / 1000;
+      
+      amp = (amp + ampFilter * (filterLength - 1)) / filterLength;
+      
+      ampFilter = amp;
       
       let inphase = radsPerSample.reduce((a, c) => a + Math.sin(sample * c), 0) / radsPerSample.length;
       let outphase = radsPerSample.reduce((a, c) => a + e.polarity * Math.sin(sample * c + pos * Math.PI), 0) / radsPerSample.length;
       
       left[i] = inphase * 32767 * amp * e.amplitude;
       right[i] = outphase * 32767 * amp * e.amplitude;
+      
+      if(e.debug) {
+        left[i] = ((amp * 2) - 1) * 32767;
+        right[i] = ((pos * 2) - 1) * 32767;
+      }
       
       sample++;
     }
